d6da10c614658d1a3f66f212f0817b0f3ba63df7
Abdullah Nayer <abdullahnayer@gmail.com>
2023-12-26 15:54:46 +0200

    init

--- app/QuillEditor.tsx
+++ app/QuillEditor.tsx
@@ -12,9 +12,44 @@ const QuillEditor = () => {
   )
   const [value, setValue] = useState("")
 
+  const handleSave = async () => {
+    try {
+      // Send a POST request to the API route with the article content
+      await fetch("/article", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ content: value }),
+      });
+
+      // Optionally, you can redirect to the next route after saving
+      // history.push("/next-route");
+    } catch (error) {
+      console.error("Error saving article:", error);
+    }
+  };
+
   return (
     <div>
-      <ReactQuill theme="snow" value={value} onChange={setValue} />
+      <ReactQuill theme="snow" value={value} onChange={setValue} modules={
+        {
+          toolbar: [
+            [{ header: [1, 2, false] }],
+            ["bold", "italic", "underline", "strike", "blockquote"],
+            [
+              { list: "ordered" },
+              { list: "bullet" },
+              { indent: "-1" },
+              { indent: "+1" },
+            ],
+            ["link", "image"],
+            ["clean"],
+          ],
+        }
+      } />
+            <button onClick={handleSave}>Save</button>
+
     </div>
   )
 }
