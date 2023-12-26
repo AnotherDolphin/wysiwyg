f9ad911e8b39f40a71afa9e9b33f244ef26b29ce
Abdullah Nayer <abdullahnayer@gmail.com>
2023-12-26 16:01:47 +0200

    db init

--- app/article/route.ts
+++ app/article/route.ts
@@ -1,17 +1,29 @@
+import db from "../db-conn"
+
 export async function GET(request: Request) {}
- 
+
 export async function HEAD(request: Request) {}
- 
+
 export async function POST(request: Request) {
-    const { title, content } = await request.json();
-    return new Response(`Article ${title} created with content ${content}`);
+  const { title, content } = await request.json()
+
+  // Get a reference to the articles collection in the database
+  const articlesCollection = db.collection("articles")
+
+  // Create a new document to insert into the collection
+  const article = { title, content }
+
+  // Insert the article document into the collection
+  await articlesCollection.insertOne(article)
+
+  return new Response(`Article ${title} created with content ${content}`)
 }
 
 export async function PUT(request: Request) {}
- 
+
 export async function DELETE(request: Request) {}
 
 export async function PATCH(request: Request) {}
- 
+
 // If `OPTIONS` is not defined, Next.js will automatically implement `OPTIONS` and  set the appropriate Response `Allow` header depending on the other methods defined in the route handler.
 export async function OPTIONS(request: Request) {}
---
+++ app/db-conn.ts
@@ -0,0 +1,17 @@
+import { MongoClient } from "mongodb";
+
+// Connection URL and database name
+const url = "mongodb://localhost:27017";
+const dbName = "wysiwyg";
+
+// Create a new MongoClient
+const client = new MongoClient(url);
+
+// Connect to the MongoDB server
+await client.connect();
+
+// Get a reference to the database
+const db = client.db(dbName);
+
+// Export the database connection for use in other modules
+export default db;
